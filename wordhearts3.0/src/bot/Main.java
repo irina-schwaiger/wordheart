c338fc82a7a8e17dcd48bc538bda2e83ae6a119c
irina <irinaschwaiger@hotmail.com>
2025-07-04 08:47:32 +0200

    Rehaul the Third will be commited and pushed now, before testing begins.

--- wordhearts3.0/src/bot/Main.java
+++ wordhearts3.0/src/bot/Main.java
@@ -1,14 +1,19 @@
 package bot;
 
-import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.generics.LongPollingBot;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
-    public static void main(String[] args) throws TelegramApiException {
-        ApiContextInitializer.init();
-        TelegramBotsApi api = new TelegramBotsApi();
-        api.registerBot((LongPollingBot) new WordHearts());
+    public static void main(String[] args) {
+        try {
+            // Initialisiert intern alles – kein ApiContextInitializer mehr
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            // registerBot nimmt hier direkt deine TelegramLongPollingBot-Instanz
+            botsApi.registerBot(new WordHearts());
+            System.out.println("Bot gestartet!");
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
